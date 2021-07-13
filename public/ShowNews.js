dd9d891eaf5bb3094a1aadae93e4f35a9e7fca9e
bodya18 <bodya18x@mail.ru>
2021-07-13 11:43:12 +0300

    add render comments in list news

--- public/ShowNews.js
+++ public/ShowNews.js
@@ -14,7 +14,7 @@ addEventListener('load', ()=>{
 })
 let listValue = 0
 let isNews = true   
-window.addEventListener('scroll', ()=>{
+window.addEventListener('scroll', (e)=>{
     let pix = document.body.scrollHeight - window.innerHeight - $(this).scrollTop()
     
     if(pix < 500 && isNews === true && listValue<AllNews.news.length){
@@ -132,123 +132,151 @@ window.addEventListener('scroll', ()=>{
         
         setTimeout(() => {
             ThisNewsId = JSON.parse(newsId).newsId
+            addMoreList(e, ThisNewsId, 0)
             
-            
-            let request2 = new XMLHttpRequest();
-            let newsId1 = JSON.stringify({id: ThisNewsId});
-            
-            request2.open("post", "/api/comments", true)
-            
-            request2.setRequestHeader("Content-Type", "application/json");
-            
-            request2.addEventListener("load", function () {
-                let session = JSON.parse(request2.response);
-                let b = document.getElementById(`ShowComments`+ThisNewsId)
-            
-                for (let i = 0; i < session.comments.length; i++) {
-                    
-                    var div = document.createElement('div')
-                    div.className = "commented-section mt-2"
-                    div.id ="comment"+ countComments
-                    for (let j = 0; j < session.users.length; j++) {
-                        if(session.users[j].id === session.comments[i].userId){
-                            if(!session.users[j].avatarURL)
-                                session.users[j].avatarURL='images/no_avatar.png'
-                            if(session.user){
-                                if(session.user.id === session.comments[i].userId){
-                                    div.innerHTML =`
-                                    <div class="bg-white p-2">
-                                        <div class="d-flex flex-row user-info">   
-                                            <img id="UserAvatar" class="rounded-circle" src="../../${session.users[j].avatarURL}" style="width: 50px; height: 50px;">
-                                            <div class="d-flex flex-column justify-content-start ml-2">
-                                                <span class="d-block font-weight-bold name">${session.users[j].name}</span>
-                                                <span class="date text-black-50">${session.comments[i].date}</span>
-                                            </div>
-                                                <form name="editComment${countComments}">
-                                                    <input type="hidden" id="commentId${countComments}" name="id" value="${session.comments[i].id}">
-                                                    <input type="hidden" id="ThisComment${countComments}" name="comment" value="${session.comments[i].comment}">
-                                                    <button id='Editcom${countComments}' onclick="editComment(${countComments}, event)" style="margin-left:400px; margin-right: 20px;" type="submit" class="btn btn-dark">Редактировать</button>
-                                                </form>
-                                                <form name="DeleteComment${countComments}">
-                                                    <input id="commentId${countComments}" type="hidden" value="${session.comments[i].id}">
-                                                    <button onclick="DelComment(${countComments}, event)" id="DelComm${countComments}" type="submit" class="btn btn-danger">Удалить</button>
-                                                </form>
-                                            </div>
-                                        </div>
-                                        <div class="mt-2">
-                                            <pre>${session.comments[i].comment}</pre>
-                                        </div>
-                                    </div><hr>`
-                                }
-                                else{
-                                    let permis = true
-                                    for (let k = 0; k < session.permissionsList.length; k++) {
-                                        if(session.permissionsList[k] === 'GIVE'){
-                                            div.innerHTML =`
-                                            <div class="bg-white p-2">
-                                                <div class="d-flex flex-row user-info">   
-                                                    <img id="UserAvatar" class="rounded-circle" src="../../${session.users[j].avatarURL}" style="width: 50px; height: 50px;">
-                                                    <div class="d-flex flex-column justify-content-start ml-2">
-                                                        <span class="d-block font-weight-bold name">${session.users[j].name}</span>
-                                                        <span class="date text-black-50">${session.comments[i].date}</span>
-                                                    </div>
-                                                        <form name="DeleteComment${countComments}">
-                                                            <input id="commentId${countComments}" type="hidden" value="${session.comments[i].id}">
-                                                            <button onclick="DelComment(${countComments}, event)" id="DelComm${countComments}" type="submit" style="margin-left:562px;" class="btn btn-danger">Удалить</button>
-                                                        </form>
-                                                    </div>
-                                                </div>
-                                                <div class="mt-2">
-                                                    <p class="comment-text">${session.comments[i].comment}</p>
-                                                </div>
-                                            </div><hr>`
-                                            permis = false
-                                        }   
-                                    }
-                                    if(permis)
-                                        div.innerHTML =`
-                                            <div class="bg-white p-2">
-                                                <div class="d-flex flex-row user-info">   
-                                                    <img id="UserAvatar" class="rounded-circle" src="../../${session.users[j].avatarURL}" style="width: 50px; height: 50px;">
-                                                    <div class="d-flex flex-column justify-content-start ml-2">
-                                                        <span class="d-block font-weight-bold name">${session.users[j].name}</span>
-                                                        <span class="date text-black-50">${session.comments[i].date}</span>
-                                                    </div>
-                                                    </div>
-                                                </div>
-                                                <div class="mt-2">
-                                                    <p class="comment-text">${session.comments[i].comment}</p>
-                                                </div>
-                                            </div><hr>`
-                                }
-                            }
-                            else{
-                                div.innerHTML =`
-                                    <div class="bg-white p-2">
-                                        <div class="d-flex flex-row user-info">   
-                                            <img id="UserAvatar" class="rounded-circle" src="../../${session.users[j].avatarURL}" style="width: 50px; height: 50px;">
-                                            <div class="d-flex flex-column justify-content-start ml-2">
-                                                <span class="d-block font-weight-bold name">${session.users[j].name}</span>
-                                                <span class="date text-black-50">${session.comments[i].date}</span>
-                                            </div>
-                                            </div>
-                                        </div>
-                                        <div class="mt-2">
-                                            <p class="comment-text">${session.comments[i].comment}</p>
-                                        </div>
-                                    </div><hr>`
-                            }
-                        }
-                    }
-                    b.appendChild(div)
-                    countComments++;
-                }
-            });
-            
-            request2.send(newsId1);    
+                
         }, 100);
         
                 
         isNews = false
     }	
 })
+
+function addMoreList(e, ThisNewsId, ThisCommentsLength){
+    e.preventDefault()
+    if (document.getElementById('button_more'+ThisNewsId)) {
+        document.getElementById('button_more'+ThisNewsId).remove()
+    }
+    let request2 = new XMLHttpRequest();
+    let newsId1 = JSON.stringify({id: ThisNewsId});
+    
+    request2.open("post", "/api/comments", true)
+    
+    request2.setRequestHeader("Content-Type", "application/json");
+    
+    request2.addEventListener("load", function () {
+        let session = JSON.parse(request2.response);
+        let b = document.getElementById(`ShowComments`+ThisNewsId)
+        if(session.comments.length > ThisCommentsLength){
+            let step = ThisCommentsLength + CountShowComments
+            if(session.comments.length - ThisCommentsLength < CountShowComments){
+                step = session.comments.length - ThisCommentsLength + CountShowComments
+            }
+            console.log(ThisCommentsLength);
+            console.log(step);
+            for (let i = ThisCommentsLength; i < step; i++) {
+                
+                var div = document.createElement('div')
+                div.className = "commented-section mt-2"
+                div.id ="comment"+ countComments
+                for (let j = 0; j < session.users.length; j++) {
+                    
+                    if(session.users[j].id === session.comments[i].userId){
+                        if(!session.users[j].avatarURL)
+                            session.users[j].avatarURL='images/no_avatar.png'
+                        if(session.user){
+                            if(session.user.id === session.comments[i].userId){
+                                div.innerHTML =`
+                                <div class="bg-white p-2">
+                                    <div class="d-flex flex-row user-info">   
+                                        <img id="UserAvatar" class="rounded-circle" src="../../${session.users[j].avatarURL}" style="width: 50px; height: 50px;">
+                                        <div class="d-flex flex-column justify-content-start ml-2">
+                                            <span class="d-block font-weight-bold name">${session.users[j].name}</span>
+                                            <span class="date text-black-50">${session.comments[i].date}</span>
+                                        </div>
+                                            <form name="editComment${countComments}">
+                                                <input type="hidden" id="commentId${countComments}" name="id" value="${session.comments[i].id}">
+                                                <input type="hidden" id="ThisComment${countComments}" name="comment" value="${session.comments[i].comment}">
+                                                <button id='Editcom${countComments}' onclick="editComment(${countComments}, event)" style="margin-left:400px; margin-right: 20px;" type="submit" class="btn btn-dark">Редактировать</button>
+                                            </form>
+                                            <form name="DeleteComment${countComments}">
+                                                <input id="commentId${countComments}" type="hidden" value="${session.comments[i].id}">
+                                                <button onclick="DelComment(${countComments}, event)" id="DelComm${countComments}" type="submit" class="btn btn-danger">Удалить</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                    <div class="mt-2">
+                                        <pre>${session.comments[i].comment}</pre>
+                                    </div>
+                                </div><hr>`
+                            }
+                            else{
+                                let permis = true
+                                for (let k = 0; k < session.permissionsList.length; k++) {
+                                    if(session.permissionsList[k] === 'GIVE'){
+                                        div.innerHTML =`
+                                        <div class="bg-white p-2">
+                                            <div class="d-flex flex-row user-info">   
+                                                <img id="UserAvatar" class="rounded-circle" src="../../${session.users[j].avatarURL}" style="width: 50px; height: 50px;">
+                                                <div class="d-flex flex-column justify-content-start ml-2">
+                                                    <span class="d-block font-weight-bold name">${session.users[j].name}</span>
+                                                    <span class="date text-black-50">${session.comments[i].date}</span>
+                                                </div>
+                                                    <form name="DeleteComment${countComments}">
+                                                        <input id="commentId${countComments}" type="hidden" value="${session.comments[i].id}">
+                                                        <button onclick="DelComment(${countComments}, event)" id="DelComm${countComments}" type="submit" style="margin-left:562px;" class="btn btn-danger">Удалить</button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                            <div class="mt-2">
+                                                <p class="comment-text">${session.comments[i].comment}</p>
+                                            </div>
+                                        </div><hr>`
+                                        permis = false
+                                    }   
+                                }
+                                if(permis)
+                                    div.innerHTML =`
+                                        <div class="bg-white p-2">
+                                            <div class="d-flex flex-row user-info">   
+                                                <img id="UserAvatar" class="rounded-circle" src="../../${session.users[j].avatarURL}" style="width: 50px; height: 50px;">
+                                                <div class="d-flex flex-column justify-content-start ml-2">
+                                                    <span class="d-block font-weight-bold name">${session.users[j].name}</span>
+                                                    <span class="date text-black-50">${session.comments[i].date}</span>
+                                                </div>
+                                                </div>
+                                            </div>
+                                            <div class="mt-2">
+                                                <p class="comment-text">${session.comments[i].comment}</p>
+                                            </div>
+                                        </div><hr>`
+                            }
+                        }
+                        else{
+                            div.innerHTML =`
+                                <div class="bg-white p-2">
+                                    <div class="d-flex flex-row user-info">   
+                                        <img id="UserAvatar" class="rounded-circle" src="../../${session.users[j].avatarURL}" style="width: 50px; height: 50px;">
+                                        <div class="d-flex flex-column justify-content-start ml-2">
+                                            <span class="d-block font-weight-bold name">${session.users[j].name}</span>
+                                            <span class="date text-black-50">${session.comments[i].date}</span>
+                                        </div>
+                                        </div>
+                                    </div>
+                                    <div class="mt-2">
+                                        <p class="comment-text">${session.comments[i].comment}</p>
+                                    </div>
+                                </div><hr>`
+                        }
+                        
+                    }
+                }
+                b.appendChild(div)
+                countComments++;
+            }
+            ThisCommentsLength += CountShowComments;
+            if(ThisCommentsLength < session.comments.length){
+                var btn = document.createElement("button");
+                btn.type = 'submit';
+                btn.id = 'button_more'+ThisNewsId
+                btn.setAttribute("onclick",`addMoreList(event, ${ThisNewsId}, ${ThisCommentsLength})`);
+                btn.className = 'btn btn-info'
+                var t = document.createTextNode("Загрузить еще");
+                btn.appendChild(t)
+                b.after(btn)
+            }
+        }
+    });
+    
+    request2.send(newsId1);
+}
