2e6299ee523fa2cdd7f4d1311f251d6f93ca4666
bodya18 <bodya18x@mail.ru>
2021-08-03 14:25:34 +0300

    add permission model

--- models/MySQL+QueryBuilder/permission.js
+++ models/MySQL+QueryBuilder/permission.js
@@ -3,66 +3,68 @@ const pool = require('../../middleware/pool')
 class Permission{
 
     async CreatePermission(permission) {
-        try{
-            await pool.query('Insert into Permissions (permission) values (?)', [permission])
-        }
-        catch (e){
-            console.log(e)
-        }
+        const qb = await pool.get_connection();
+        await qb
+            .insert('Permissions', {permission})
+        qb.release();
     }
 
     async DeletePermission(id){
         var temp = true
-        await pool.query('select * from Rule_Permission where permissionId=?', [id])
-            .then(data=>{
-                if (!data[0][0])
-                    pool.query('delete from Permissions where id=?', [id])
-                else temp = false
-            })
-            .catch(e => {
-                console.log(e);
-            })
-            return temp
+        const qb = await pool.get_connection();
+        const response = await qb
+            .select('*')
+            .where({permissionId: id})
+            .get('Rule_Permission');
+        if(!response[0])
+            await qb.delete('Permissions', {id})
+        else
+            temp = false
+        qb.release();
+        return temp
     }
 
     async ShowAllPermissions(id){
+        const qb = await pool.get_connection();
         var temp = []
-        await pool.query(`Select DISTINCT Permissions.permission
-        from Rules, Permissions, users, Rule_Permission, Rule_User 
-        where Rule_Permission.ruleId = Rules.id AND Rule_Permission.permissionId = Permissions.id AND Rule_User.userId = users.id AND Rule_User.ruleId = Rules.id AND users.id = ?`,[id])
-                .then(data => {
-                    for(let i = 0;i<data[0].length;i++){
-                        temp[i] = data[0][i].permission
-                    }
-                })
-                .catch(e=>{
-                    return console.log(e)
-                })
+        await qb
+            .distinct()
+            .from('Rules')
+            .select('Permissions.permission')
+            .join('Rule_Permission', 'Rule_Permission.ruleId=Rules.id')
+            .join('Permissions', 'Rule_Permission.permissionId=Permissions.id ')
+            .join('Rule_User', 'Rule_User.ruleId=Rules.id')
+            .join('users', 'Rule_User.userId=users.id')
+            .where('users.id', id)
+            .get((err, data)=>{
+                for(let i = 0;i<data.length;i++){
+                    temp[i] = data[i].permission
+                }
+            });
+        qb.release()
         return temp;
     }
 
     async GetPermissions(){
-        var temp
-        await pool.query('Select * from Permissions')
-            .then(rule => {
-                temp = rule[0]
-            })
-            .catch(e =>{
-                return console.log(e);
-            })
-        return(temp)
+        const qb = await pool.get_connection();
+        const response = await qb
+            .select('*')
+            .get('Permissions')
+        return response
     }
 
     async GetPermissionBy(what, value){
-        var temp
-        await pool.query(`SELECT * FROM Permissions where ${what} = ?`, [value])
-            .then(data =>{
-                temp = data[0][0]
-            })
-            .catch(e =>{
-                return console.log(e)
-            })
-        return temp;
+        let data = {what: value}
+        data = JSON.stringify(data)
+        data = data.replace(/".*?"/, `"${what}"`)
+        data = JSON.parse(data)
+        const qb = await pool.get_connection();
+        const response = await qb
+            .select('*')
+            .where(data)
+            .get('Permissions');
+        qb.release();
+        return response[0]
     }
 }
 
