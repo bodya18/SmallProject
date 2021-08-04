0d3ce2c2815dbd3ca7db785e5ade0051d5d2d4a1
bodya18 <bodya18x@mail.ru>
2021-08-04 16:43:48 +0300

    fix

--- middleware/pool.js
+++ middleware/pool.js
@@ -1,21 +1,17 @@
 const configMiggleware = require('./config')
 
+const settings = {
+    host: configMiggleware.host,
+    database: 'usersdb',
+    user: 'root',
+    password: 'ZAQwsxz1.'
+};
+
 if(configMiggleware.database === 'MySQL'){
     const mysql = require('mysql2')
-    module.exports = mysql.createConnection({
-        host: configMiggleware.host,
-        user: 'root',
-        database: 'usersdb',
-        password:'ZAQwsxz1.'
-    }).promise()
+    module.exports = mysql.createConnection(settings).promise()
 }
-else{
+if(configMiggleware.database === 'MySQL+QueryBuilder'){
     const QueryBuilder = require('node-querybuilder');
-    const settings = {
-        host: configMiggleware.host,
-        database: 'usersdb',
-        user: 'root',
-        password: 'ZAQwsxz1.'
-    };
     module.exports = new QueryBuilder(settings, 'mysql', 'pool');
 }
